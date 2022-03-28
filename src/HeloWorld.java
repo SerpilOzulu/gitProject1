6a2a8b0dbebe45fc82e4d6e65b9b41cb92443bd1
Serpil Ozulu <serpilozturk000@gmail.com>
2022-03-28 23:20:53 +0300

    added more work

--- src/HeloWorld.java
+++ src/HeloWorld.java
@@ -1,6 +1,8 @@
 public class HeloWorld {
     public static void main(String[] args) {
         System.out.println("work1 started");
+        System.out.println("created git repository");
+        System.out.println("added");
     }
 
 }
